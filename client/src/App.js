8a159bb3da379858e0099c6cffa377af44175077
adudley78 <dudleyadam@gmail.com>
2019-11-19 14:05:31 -0500

    Build register and login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import './App.css';
@@ -18,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exaxt path='/' component={Home} />
                 <Route exaxt path='/about' component={About} />
+                <Route exaxt path='/register' component={Register} />
+                <Route exaxt path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
