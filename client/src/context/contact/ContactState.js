a4e53704159431759bcc8933a1414107dc83475d
adudley78 <dudleyadam@gmail.com>
2019-12-23 11:52:49 -0500

    Build update contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,6 +63,26 @@ const ContactState = props => {
     }
   };
 
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
   };
@@ -73,10 +93,6 @@ const ContactState = props => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   const filterContacts = text => {
